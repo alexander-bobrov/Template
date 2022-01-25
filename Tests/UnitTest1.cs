bb7e646053c4e2ea84208eae335ed884083a249d
Alexander Bobrov <alexander.bobrov.en@gmail.com>
2022-01-26 01:42:27 +0300

    ...

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,18 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Template;
 
 namespace Tests
 {
-    public class Tests
+    public class AccountTests
     {
+        private TestServer server;
+        private HttpClient client;
+
         [SetUp]
         public void Setup()
         {
+            server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            client = server.CreateClient();
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1()
         {
-            Assert.Pass();
+            var r = await client.GetAsync("/");
         }
     }
 }
